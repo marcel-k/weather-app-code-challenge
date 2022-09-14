53ab7a147195869781e1cd9be6320ef5759fda64
marcel-k <marvdkamp@hotmail.com>
2022-09-14 22:49:05 +0200

    geen mediaquery meer nodig

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -5,20 +5,12 @@ export const Reset = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    color: #58595b;
+    color: #4e4a7c;
   }
 `;
 
 export const App = styled.div`
-  padding: 0 0.75rem;
-
-  @media only screen and (min-width: 600px) {
-    padding: 0 0.75rem 0 4.75rem;
-  }
-
-  /* @media only screen and (min-width: 1921px) {
-    padding: 0 0.75rem 0 13.25rem;
-  } */
+  padding: 2rem;
 `;
 
 export const Typography = createGlobalStyle`
