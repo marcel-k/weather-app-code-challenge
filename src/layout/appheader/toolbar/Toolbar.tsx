54e8a7892627f84d2c3644c30bf6969d0736e7ac
marcel-k <marvdkamp@hotmail.com>
2022-09-14 22:49:16 +0200

    toolbar iconbuttons

--- src/layout/appheader/toolbar/Toolbar.tsx
+++ src/layout/appheader/toolbar/Toolbar.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { IconButton } from "../../../components";
 import * as S from './ToolbarStyle';
 
 interface ToolbarProps {
@@ -8,8 +9,8 @@ interface ToolbarProps {
 export const Toolbar: FC<ToolbarProps> = (props) => {
   return (
     <S.Toolbar role={'toolbar'}>
-      <div>switch</div>
-      <div>locationsearch</div>
+      <IconButton iconName="dark_mode" onClick={() => {}} />
+      <IconButton iconName="search" onClick={() => {}} />
     </S.Toolbar>
   )
 };
