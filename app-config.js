dea1de92b00ef1e25d56a39f16c0a48cd1f42da4
marcel-k <marvdkamp@hotmail.com>
2022-09-17 22:34:32 +0200

    Updates

--- app-config.js
+++ app-config.js
@@ -2,6 +2,7 @@ const appConfig = {
   "useMockData": false,
   "maxDaysForecast": 5,
   "environment": "development",
+  "urlRoot": "/weather-app-code-challenge/",
   "apiKey": "f5f602b5811d444692ab37bbeee62b23",
   "weatherApiUrl": "https://api.openweathermap.org/data/2.5/weather",
   "forecastApiUrl": "https://api.openweathermap.org/data/2.5/forecast",
