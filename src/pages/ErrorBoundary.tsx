b2ae75fe226ff75f0cd32c0820a8a27745269d50
marcel-k <marvdkamp@hotmail.com>
2022-09-17 19:45:18 +0200

    generieke error boundary

---
+++ src/pages/ErrorBoundary.tsx
@@ -0,0 +1,61 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+import styled from "styled-components";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+  error: Error | null;
+}
+
+const StyledError = styled.div`
+  display: flex;
+  padding-bottom: 1rem;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  
+ > h2 {
+   color: red;
+  }
+`;
+
+/**
+ * Generic error boundary that catches any unforeseen 
+ * errors of a page component and displays an error message.
+ */
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+    error: null
+  };
+
+  public static getDerivedStateFromError(error: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <StyledError>
+            <h2>An error occured while loading the page. Please reload and try again.</h2>
+            <p>Errormessage: {this.state.error?.message}</p>
+          </StyledError>
+          {this.props.children}
+        </>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 
 import { DayCard, TemperatureCard } from "../../features";
+import ErrorBoundary from "../ErrorBoundary";
 import * as PS from "../PageStyle";
 import * as HS from "./HomeStyle";
 
@@ -11,44 +12,46 @@ interface HomeProps {
 export const Home: FC<HomeProps> = (props) => {
   return (
     <PS.Page>
-      <HS.Home>
-        <HS.Grid>
-          <DayCard
-            height={'19rem'}
-            forecast={false}
-            elevated={false}
-            id={'card-weather-today'}
-            backgroundColor={'#6452ef'}
-            backgroundImageUrl={'assets/images/card_background-374-314.png'}
-          />
-          <TemperatureCard 
-            height={'19rem'}
-            elevated={false}
-            backgroundColor={'#3addb1'}
-            id={'card-temperature-and-humidity'}
-          />
-          <HS.CardScroller>
-            <HS.CardScrollerTitle>14 Day Weather Forecast</HS.CardScrollerTitle>
-            <HS.CardScrollerContent>
-              {
-                [
-                  '#f82298', '#f9b423', '#219bfa', '#da2221', '#2162f8', '#3addb1', '#6452ef',
-                  '#f82298', '#f9b423', '#219bfa', '#da2221', '#2162f8', '#3addb1', '#6452ef',
-                ]
-                  .map((color, index) =>
-                    <DayCard
-                      height={'13.25rem'}
-                      placeholder={index > 4}
-                      backgroundColor={color}
-                      daysInTheFuture={index + 1}
-                      id={`card-weather-forecast-${index + 1}`}
-                      key={`card-weather-forecast-${index + 1}`}
-                    />
-                  )}
-            </HS.CardScrollerContent>
-          </HS.CardScroller>
-        </HS.Grid>
-      </HS.Home>
-    </PS.Page>
+        <HS.Home>
+        <ErrorBoundary>
+          <HS.Grid>
+            <DayCard
+              height={'19rem'}
+              forecast={false}
+              elevated={false}
+              id={'card-weather-today'}
+              backgroundColor={'#6452ef'}
+              backgroundImageUrl={'assets/images/card_background-374-314.png'}
+            />
+            <TemperatureCard
+              height={'19rem'}
+              elevated={false}
+              backgroundColor={'#3addb1'}
+              id={'card-temperature-and-humidity'}
+            />
+            <HS.CardScroller>
+              <HS.CardScrollerTitle>14 Day Weather Forecast</HS.CardScrollerTitle>
+              <HS.CardScrollerContent>
+                {
+                  [
+                    '#f82298', '#f9b423', '#219bfa', '#da2221', '#2162f8', '#3addb1', '#6452ef',
+                    '#f82298', '#f9b423', '#219bfa', '#da2221', '#2162f8', '#3addb1', '#6452ef',
+                  ]
+                    .map((color, index) =>
+                      <DayCard
+                        height={'13.25rem'}
+                        placeholder={index > 4}
+                        backgroundColor={color}
+                        daysInTheFuture={index + 1}
+                        id={`card-weather-forecast-${index + 1}`}
+                        key={`card-weather-forecast-${index + 1}`}
+                      />
+                    )}
+              </HS.CardScrollerContent>
+            </HS.CardScroller>
+          </HS.Grid>
+    </ErrorBoundary>
+        </HS.Home>
+      </PS.Page>
   )
 };
