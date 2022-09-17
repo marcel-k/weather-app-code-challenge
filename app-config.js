c4314b796a74aac4a7cdfc1474437068795a98a3
marcel-k <marvdkamp@hotmail.com>
2022-09-17 22:18:24 +0200

    Updates

---
+++ app-config.js
@@ -0,0 +1,11 @@
+const appConfig = {
+  "useMockData": false,
+  "maxDaysForecast": 5,
+  "environment": "development",
+  "apiKey": "f5f602b5811d444692ab37bbeee62b23",
+  "weatherApiUrl": "https://api.openweathermap.org/data/2.5/weather",
+  "forecastApiUrl": "https://api.openweathermap.org/data/2.5/forecast",
+  "reverseGeocodingApiUrl": "https://api.openweathermap.org/geo/1.0/reverse",
+};
+
+window.appConfig = appConfig;
