dcd4b8bb1b04a3cfc2ce12d821702fa4e68f2cfb
marcel-k <marvdkamp@hotmail.com>
2022-09-18 22:07:49 +0200

    cleanup

--- src/components/card/Card.test.tsx
+++ src/components/card/Card.test.tsx
@@ -1,5 +1,6 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+
 import { Card } from './Card';
 
 test('renders a default card', () => {
