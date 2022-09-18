8c0773d54569b23cc9f6df3ad29514a12dad0963
marcel-k <marvdkamp@hotmail.com>
2022-09-18 20:53:54 +0200

    simple card snapshot test

--- src/components/card/Card.test.tsx
+++ src/components/card/Card.test.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Card } from './Card';
 
-test('renders todo', () => {
-  // render(<AppHeader />);
-  const titleElement = screen.getByText(/todo/i);
-  expect(titleElement).toBeInTheDocument();
+test('renders a default card', () => {
+  const { container: card } = render(<Card>Some content</Card>);
+
+  expect(card).toMatchInlineSnapshot(`
+<div>
+  <div
+    class="sc-bczRLJ hcceGu"
+  >
+    Some content
+  </div>
+</div>
+`);
 });
